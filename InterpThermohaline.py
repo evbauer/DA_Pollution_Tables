5bae17710621ab5b262764f2a52d51d1ec05adc3
Evan <branbauep@gmail.com>
2018-12-16 19:08:33 -0800

    refine logg for interpolation

--- InterpThermohaline.py
+++ InterpThermohaline.py
@@ -23,7 +23,10 @@ fs09 = []
 # need to read the slightly different value from the MESA model that has cooled a bit over 100tau_diff
 actual_Teffs038 = [] 
 actual_Teffs06 = [] 
-actual_Teffs09 = [] 
+actual_Teffs09 = []
+loggs038 = []
+loggs06 = []
+loggs09 = []
 
 for entry in Teffs:
     label = str(entry) + 'K'
@@ -32,11 +35,12 @@ for entry in Teffs:
     d = np.genfromtxt(filename)
     mdot = d[:,0]
     XCa038 = d[:,1]
-    diff = d[:,2] # unused, prediction of diffusion
     teff038 = d[0,3]
+    logg038 = d[0,4]
     # Interpolating log XCa as a function of log Mdot
     fs038.append(interpolate.interp1d(np.log10(XCa038),np.log10(mdot),fill_value='extrapolate'))
     actual_Teffs038.append(teff038)
+    loggs038.append(logg038)
 
     filename = 'thermohaline_surfaceX/WD_M_0.6/' + label + '.data'
     d = np.genfromtxt(filename)
@@ -44,24 +48,35 @@ for entry in Teffs:
     XCa06 = d[:,1]
     diff = d[:,2] # unused, prediction of diffusion
     teff06 = d[0,3]
+    logg06 = d[0,4]
     # Interpolating log XCa as a function of log Mdot
     fs06.append(interpolate.interp1d(np.log10(XCa06),np.log10(mdot),fill_value='extrapolate'))
     actual_Teffs06.append(teff06)
+    loggs06.append(logg06)
 
     filename = 'thermohaline_surfaceX/WD_M_0.9/' + label + '.data'
     d = np.genfromtxt(filename)
     mdot = d[:,0]
     XCa09 = d[:,1]
-    diff = d[:,2] # unused, prediction of diffusion
     teff09 = d[0,3]
+    logg09 = d[0,4]
     # Interpolating log XCa as a function of log Mdot
     fs09.append(interpolate.interp1d(np.log10(XCa09),np.log10(mdot),fill_value='extrapolate'))
     actual_Teffs09.append(teff09)
+    loggs09.append(logg09)
 
+
+# Get logg as a function of Teff for each mass
+g038 = interpolate.interp1d(actual_Teffs038,loggs038,fill_value='extrapolate')
+g06 = interpolate.interp1d(actual_Teffs06,loggs06,fill_value='extrapolate')
+g09 = interpolate.interp1d(actual_Teffs09,loggs09,fill_value='extrapolate')
+
+
+# Interpolate to find mdot assuming fixed mass for each mass.
+# Builds a temporary interpolator at fixed log_XCa each time.
+# Inefficient, but seems robust, and I don't need it to be fast.
 
 def log_mdot038(log_XCa,Teff):
-    # Build a temporary interpolator at fixed log_XCa each time.
-    # Inefficient, but seems robust, and I don't need it to be fast.
     log_mdots_for_this_XCa = []
     for logmdot in fs038:
         log_mdots_for_this_XCa.append(logmdot(log_XCa))
@@ -69,8 +84,6 @@ def log_mdot038(log_XCa,Teff):
     return f(Teff)
 
 def log_mdot06(log_XCa,Teff):
-    # Build a temporary interpolator at fixed log_XCa each time.
-    # Inefficient, but seems robust, and I don't need it to be fast.
     log_mdots_for_this_XCa = []
     for logmdot in fs06:
         log_mdots_for_this_XCa.append(logmdot(log_XCa))
@@ -78,8 +91,6 @@ def log_mdot06(log_XCa,Teff):
     return f(Teff)
 
 def log_mdot09(log_XCa,Teff):
-    # Build a temporary interpolator at fixed log_XCa each time.
-    # Inefficient, but seems robust, and I don't need it to be fast.
     log_mdots_for_this_XCa = []
     for logmdot in fs09:
         log_mdots_for_this_XCa.append(logmdot(log_XCa))
@@ -89,8 +100,8 @@ def log_mdot09(log_XCa,Teff):
 
 def log_mdot(log_XCa,Teff,logg):
     # Interpolate the other three functions in logg at this Teff
-    f = interpolate.interp1d([7.5,8.0,8.5],
-                             [log_mdot038(log_XCa,Teff),log_mdot06(log_XCa,Teff),log_mdot09(log_XCa,Teff)],
+    f = interpolate.interp1d([g038(Teff), g06(Teff), g09(Teff)],
+                             [log_mdot038(log_XCa,Teff), log_mdot06(log_XCa,Teff), log_mdot09(log_XCa,Teff)],
                              fill_value='extrapolate')
     return f(logg)
 
